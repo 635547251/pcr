94b98e3c5739841546f7d66e35bb436ca4b26a25
phil666 <635547251@qq.com>
2021-01-20 13:58:44 +0800

    删除matplotlib模块

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import os
 from collections import defaultdict
 from multiprocessing import Manager, Process
 
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import ticker
-
-from .config import data_path, proc_pool_size, ranking
+from .config import data_path, proc_pool_size, start_time
 from .db import get_pcr_team
 
 with open("pcr/conf/ch.json", "r", encoding="utf-8") as f:
@@ -17,11 +13,6 @@ with open("pcr/conf/ch.json", "r", encoding="utf-8") as f:
     main_tank = d["main_tank"]
     other_list = d["other_list"]
     all_ch_num = len(d["pos2ch_dic"])
-
-
-# 这两行代码解决 plt 中文显示的问题
-plt.rcParams['font.sans-serif'] = ['SimHei']
-plt.rcParams['axes.unicode_minus'] = False
 
 
 def get_ch_attend_and_win(pcr_team: list[tuple[str]]) -> dict[str, tuple[dict[str, str]]]:
@@ -85,92 +76,6 @@ def get_ch_attend_and_win(pcr_team: list[tuple[str]]) -> dict[str, tuple[dict[st
     return ch_attend_and_time
 
 
-def get_ch_attend_and_win_chart(pcr_team: list[tuple[str]]):
-    '''
-    获取各角色出场率胜率图表
-    :param  pcr_team    数据库查询结果
-    '''
-    def get_axis(ch_attend_time: dict[str, int], ch_win_time: dict[str, int],
-                 ch_lose_time: dict[str, int]) -> tuple[list]:
-        '''
-        获取xy轴数据
-        :param  ch_attend_time  出场次数
-        :param  ch_win_time     胜率次数
-        :parma  ch_lose_time    失败次数
-        :return y_axis          y轴 角色
-        :return x_axis1         x轴1 出场率
-        :return x_axis2         x轴2 胜率
-        '''
-        team_len = len(set(pcr_team))
-        ch_attend_rate = sorted(
-            {k: round(v / team_len, 4)
-             for k, v in ch_attend_time.items()}.items(),
-            key=lambda x: x[1])[-ranking:]
-        z = list(zip(*ch_attend_rate))
-        y_axis, x_axis1 = z[0], z[1]  # y轴 角色  x轴1 出场率
-        x_axis2 = [round(ch_win_time[ch] / (ch_win_time[ch] + ch_lose_time[ch]), 4)
-                   for ch in y_axis][::-1]  # x轴2 胜率
-
-        return y_axis, x_axis1, x_axis2
-
-    def generate_chart(y_axis: list[str], x_axis1: list[str],
-                       x_axis2: list[str], title: str, height=0.8, n=2):
-        '''
-        生成各角色出场率胜率图表
-        :param  y_axis  y轴 角色
-        :param  x_axis1 x轴1 出场率
-        :param  x_axis2 x轴2 胜率
-        :param  title   图标标题
-        :param  height  条状图总宽度
-        :param  n       条状图总个数
-        '''
-        def to_percent(temp, position):
-            return "%1.0f" % (100 * temp) + "%"
-
-        y_pos = height / n ** 2
-        plt.barh(np.arange(len(y_axis)) + y_pos, x_axis1,
-                 height=y_pos, color="r",  alpha=0.8, label="出场率")
-        plt.barh(np.arange(len(y_axis)) - y_pos, x_axis2,
-                 height=y_pos, color="g", alpha=0.7, label="胜率")
-
-        plt.title(title)  # 标题
-        plt.yticks(range(ranking), y_axis)  # y轴标签
-
-        plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(to_percent))  # x轴坐标显示百分百
-        plt.xlim([0, 1])  # x轴坐标范围
-
-        # 为每个条形图添加数值标签
-        for x, y in enumerate(x_axis1):
-            plt.text(y + 0.01, x + y_pos,
-                     "{:.2%}".format(y), va="center", ha="left", label="出场率")
-        for x, y in enumerate(x_axis2):
-            plt.text(y + 0.01, x - y_pos,
-                     "{:.2%}".format(y), va="center", ha="left", label="出场率")
-
-    fig = plt.figure(figsize=(16, 8))
-    fig.canvas.set_window_title("pcr国服各角色出场率TOP%s及胜率" % ranking)  # 窗口名
-    fig.subplots_adjust(left=0.1, right=0.97, top=0.95,
-                        bottom=0.05, wspace=0.3)  # 子图间距
-    # fig.tight_layout()  # 自动调整标签
-
-    # 各角色
-    ch_attend_and_win = get_ch_attend_and_win(pcr_team)
-    plt.subplot(221)
-    generate_chart(*get_axis(*ch_attend_and_win["1"]), title="各个角色")
-    fig.legend(loc="upper right")  # 标签
-    # 2人组合
-    plt.subplot(222)
-    generate_chart(*get_axis(*ch_attend_and_win["2"]), title="2人组合")
-    # 3人组合
-    plt.subplot(223)
-    generate_chart(*get_axis(*ch_attend_and_win["3"]), title="3人组合")
-    # 4人组合
-    plt.subplot(224)
-    generate_chart(*get_axis(*ch_attend_and_win["4"]), title="4人组合")
-
-    plt.show()
-
-
 def get_mypjjc_team(pcr_team: list[tuple[str]]):
     '''
     获取我的pjjc所有阵容
@@ -231,8 +136,8 @@ def get_mypjjc_team(pcr_team: list[tuple[str]]):
 
     # TODO 回溯算法优化 大数据存储
     filename = os.path.join(data_path, "data.txt")
-    with open(filename, "w") as fd:
-        backtrace(3, list(all_team.keys()), fd)
+    with open(filename, "w") as f:
+        backtrace(3, list(all_team.keys()), f)
 
     # 分块切割文件
     file_end = os.path.getsize(filename)
@@ -240,9 +145,9 @@ def get_mypjjc_team(pcr_team: list[tuple[str]]):
         for index, (chunk_start, size) in enumerate(chunkify(filename, file_end)):
             f.seek(chunk_start)
             lines = f.read(size).splitlines()
-            with open(os.path.join(data_path, "data" + str(index) + ".txt"), "w") as fd:
+            with open(os.path.join(data_path, "data" + str(index) + ".txt"), "w") as f:
                 for line in lines:
-                    print(line.decode("utf-8"), file=fd)
+                    print(line.decode("utf-8"), file=f)
 
 
 def get_bad_comment_rate(obj: list[list[str, int, int]]) -> float:
@@ -263,8 +168,8 @@ def analyze_team_process(filename_lst, res):
     :param  res             结果存放列表
     '''
     for filename in filename_lst:
-        with open(filename, "r") as fd:
-            for p_team in fd:
+        with open(filename, "r") as f:
+            for p_team in f:
                 p_team = eval(p_team)
                 lst, s = [], None
                 for defense_team, attack_team_data in p_team.items():
@@ -277,11 +182,8 @@ def analyze_team_process(filename_lst, res):
                     gc, bc = attack_team_data[2], attack_team_data[3]
                     lst.append([defense_team, gc, bc])
                 else:
-                    if s and get_bad_comment_rate(lst) >= 0.25:
-                        for ch in ["羊驼", "布丁", "望", "裁缝", "水吃", "yly", "充电宝", "环奈", "万圣忍", "初音", "水黑"]:
-                            if ch in s:
-                                res.append(lst)
-                                break
+                    if s:
+                        res.append(lst)
 
 
 def analyze_best_pjjc_team():
@@ -307,16 +209,15 @@ def analyze_best_pjjc_team():
     # 按差评排序从小到大
     res.sort(key=get_bad_comment_rate, reverse=True)
 
-    for r in res:
-        print("%s\t%s\t%s\t" % (r[0][0].ljust(19), r[1][0].ljust(19), r[2][0]))
+    with open("pcr/res.txt", "w") as f:
+        for r in res:
+            print(r, file=f)
+            # print("%s\t%s\t%s\t" % (r[0][0].ljust(19), r[1][0].ljust(19), r[2][0]))
 
 
 if __name__ == "__main__":
     # 获取数据库所有数据
-    pcr_team = get_pcr_team(start_time="2020-11-04")
-
-    # 各角色出场率胜率图表
-    get_ch_attend_and_win_chart(pcr_team)
+    pcr_team = get_pcr_team(start_time=start_time)
 
     # 获取我的pjjc所有阵容
     get_mypjjc_team(pcr_team)
